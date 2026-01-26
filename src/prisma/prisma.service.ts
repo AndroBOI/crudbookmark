60a009c6874d50e9feb4388659f4d7d1be8c663b
andrew <andrewjovellanos48@gmail.com>
2026-01-26 16:38:59 +0800

    changes in prisma service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,18 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    super({});
-  }
+export class PrismaService extends PrismaClient {
+  constructor(config: ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: config.get('DATABASE_URL'),
+        },
+      },
+    });
 
-  async onModuleInit() {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    await this.$connect();
+    console.log(config.get('DATABASE_URL'));
   }
 }
